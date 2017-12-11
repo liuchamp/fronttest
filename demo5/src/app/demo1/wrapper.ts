6fcfaa07a9ac3fc309bdb72a852581e1e4d24d11
LiuChamp <liucheng1010299461@163.com>
2017-12-11 16:53:55 +0800

    添加ng-content的使用说明

--- demo5/src/app/demo1/wrapper.ts
+++ demo5/src/app/demo1/wrapper.ts
@@ -10,3 +10,7 @@ import { Component } from '@angular/core';
   `
 })
 export class Wrapper { }
+/**
+ * ng-content指令，隐藏了这个标签下的子组件。和<router-outlet></router-outlet> 的功能类似，可以在页面中显示子组件的页面内容。
+ * 使用的好处是提高了代码的对外不可见性，其中的不好之处是，是的代码产生了很多歧义，是的开发调试出现问题。
+ */
