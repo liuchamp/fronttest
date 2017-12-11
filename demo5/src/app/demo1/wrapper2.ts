dca695629373a2aa6cae4dfddae4e0d870117a76
LiuChamp <liucheng1010299461@163.com>
2017-12-11 17:15:21 +0800

    添加新wrapper2

---
+++ demo5/src/app/demo1/wrapper2.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'wrapper2',
+    template: ``
+})
+
+export class Wapper2Component implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
