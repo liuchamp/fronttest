e8d64a49c8dbb74da22e75d9616e45387b2cc7ef
LiuChamp <liucheng1010299461@163.com>
2017-12-13 17:36:16 +0800

    其他Subject使用说明

--- demo5/src/app/demo2/updatemessage.service.ts
+++ demo5/src/app/demo2/updatemessage.service.ts
@@ -4,7 +4,8 @@ import { BehaviorSubject }  from 'rxjs/BehaviorSubject'
 /**
  * 用于BehaviorSubject使用展示. http://blog.csdn.net/dbs1215/article/details/52454866 
  * BehaviorSubject初始化是，必须有一个值，这是于Subject不同的地方之一
- * 
+ *  参考这篇博文，就能看清楚其使用方法。
+
  */
 @Injectable()
 export class UpdatemessageService {
@@ -20,3 +21,8 @@ export class UpdatemessageService {
         return this.obsubject.asObservable();
     }
 }
+
+/**
+ 	区别集中subject的博文： http://blog.csdn.net/kongbaidepao/article/details/51240456 
+ 	
+*/
